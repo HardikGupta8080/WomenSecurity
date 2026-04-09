c40ad83110d99e9ef811000afd01bf20d82c2974
hardik <hardikgupta8080@gmail.com>
2026-04-09 21:49:58 +0530

    fix some emergency issue

--- src/main/java/com/iiitu/WomenSecurity/model/EmergencyLocation.java
+++ src/main/java/com/iiitu/WomenSecurity/model/EmergencyLocation.java
@@ -12,8 +12,13 @@ public class EmergencyLocation {
     private String id;
 
     private String userId;
+    private String status;
     private double latitude;
     private double longitude;
+    private String note;
+    private String type;
+    private String phoneNumber;
+    private String email;
 
     private LocalDateTime time;
 
@@ -26,19 +31,60 @@ public class EmergencyLocation {
         this.time = LocalDateTime.now();
     }
 
+    public EmergencyLocation(
+            String userId,
+            String status,
+            double latitude,
+            double longitude,
+            String note,
+            String type,
+            String phoneNumber,
+            String email
+    ) {
+        this.userId = userId;
+        this.status = status;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.note = note;
+        this.type = type;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.time = LocalDateTime.now();
+    }
+
     public String getId() { return id; }
 
     public String getUserId() { return userId; }
+
+    public String getStatus() { return status; }
 
     public double getLatitude() { return latitude; }
 
     public double getLongitude() { return longitude; }
 
+    public String getNote() { return note; }
+
+    public String getType() { return type; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+
+    public String getEmail() { return email; }
+
     public LocalDateTime getTime() { return time; }
 
     public void setUserId(String userId) { this.userId = userId; }
 
+    public void setStatus(String status) { this.status = status; }
+
     public void setLatitude(double latitude) { this.latitude = latitude; }
 
     public void setLongitude(double longitude) { this.longitude = longitude; }
+
+    public void setNote(String note) { this.note = note; }
+
+    public void setType(String type) { this.type = type; }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public void setEmail(String email) { this.email = email; }
 }
